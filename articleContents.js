ff8471253b348eef8eb7a6f1f7d92cfd6db1b3a1
ben byford <benjaminbyford@gmail.com>
2014-10-23 17:25:42 +0100

    add comments to js

--- articleContents.js
+++ articleContents.js
@@ -1,20 +1,37 @@
 /*
-*   articleContents by @benbyford
+*   articleContents.js by @benbyford
 */
 $.fn.createContents = function() {
+
+    //
+    // if no .article-contents on page add contents to start of article
+    //
+
     if($('.article-contents')){
         $('.article-contents').append('<h3>Contents</h3>');
     }else{
         $(this).prepend('<div class="article-contents"></div>');
     }
 
+    //
+    //  Find headers
+    //
     $headers = $(this).find('h1, h2, h3, h4, h5');
 
     $i = 0;
     $headers.each(function(){
-        if($(this).html() != ''){
+        if($(this).html() != ''){ // don't use header if empty
+
+            //
+            // add id to headers matching anchor links in our contents
+            //
 
             $(this).attr('id', 'h'+$i);
+
+            // 
+            // add headers to contents div
+            //
+
             $('.article-contents').append('<a href="#h'+$i+'">'+$(this).html()+'</a>');
         }
         $i++;
